58572cb43192079c3dbfeed555cca9567b2355a2
Mateusz <mateuszkrolik87@gmail.com>
2024-06-05 15:13:42 +0200

    3_Implementing_POST_Method_to_create_User_Resource:
    1. Created a @PostMapping("/users") with a "createOneUser" method with a return type of "void" and a @RequestBody of User, then passed "save()" method from the "userDaoService" accepting said user to it.
    
    2. Implemented logic for user saving method in UserDao @Service, that adds and returns a user with hardcoded auto-incremented id counter, so it doesn't have to be specified when sending the request.
    
    3. Tested the EndPoint using both the "Talend" Chrome extension and RestClient in VSCode for documentation.
    
    The POST method now returns "Success" (200) Status Code, but should more specifically return  "Resource Created" (201), will be fixed in near future.

--- src/main/java/com/mateusz/rest/webservices/restful_web_services/users/UserResource.java
+++ src/main/java/com/mateusz/rest/webservices/restful_web_services/users/UserResource.java
@@ -7,6 +7,8 @@ import java.util.List;
 // import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class UserResource {
@@ -27,6 +29,12 @@ public class UserResource {
     @GetMapping("/users/{id}")
     public User retrieveOneUser(@PathVariable int id) {
         return userDaoService.findOne(id);
+    }
+
+    // POST /users
+    @PostMapping("/users")
+    public void createOneUser(@RequestBody User user) {
+        userDaoService.save(user);
     }
 
 }
