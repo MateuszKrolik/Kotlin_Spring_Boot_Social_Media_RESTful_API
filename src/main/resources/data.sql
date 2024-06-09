b4c4db9e0f6c172911e07be36e0ce685257fffbe
Mateusz <mateuszkrolik87@gmail.com>
2024-06-09 11:43:42 +0200

    15_JDBC_H2-Creating_User_Entity_and_some_test_data:
    -Configured H2 and JDBC using below fields in application.properties file: 1.spring.datasource.url=jdbc:h2:mem:testdb
    *Sets a static address for H2 Console
    2.spring.h2.console.enabled=true
    *Enables H2 Console
    3.spring.jpa.defer-datasource-initialization=true
    *Delays initialization of dataSource to avoid errors
    
    - Set up some dummy data in "src/main/resources/data.sql" for testing purposes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO user_details(id,birth_date,name)
+VALUES(10001,current_date(),'Mateusz');
+
+INSERT INTO user_details(id,birth_date,name)
+VALUES(10002,current_date(),'Mati');
+
+INSERT INTO user_details(id,birth_date,name)
+VALUES(10003,current_date(),'Mefju');
