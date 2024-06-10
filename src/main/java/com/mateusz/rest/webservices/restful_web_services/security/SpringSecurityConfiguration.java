1f3ac7a42287011f55775e8a0f34c1e97fff83fc
Mateusz <mateuszkrolik87@gmail.com>
2024-06-10 15:51:46 +0200

    22_Enhancing_Spring_Security_Configuration_for_Basic_Authentication:
    1. Added a security package with a SpringSecurity @Configuration, that  has a @Bean Overriding the "SecurityFilterChain" accepting  http variable of type "HttpSecurity".
    
    2. On the http variable added DSL Lambda Expressions for:
    
    - Authorizing Http Request, that requires all requests to be authenticated.
    
    - HttpBasic(withDefaults) that overrides the default login page user with basic auth pop-up instead
    
    - Disabling CSRF, which negatively affected POST & PUT Routes with a 403 (Forbidden) Error.

---
+++ src/main/java/com/mateusz/rest/webservices/restful_web_services/security/SpringSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.mateusz.rest.webservices.restful_web_services.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+                http
+                                // Lambda DSL w/ "authz" meaning authorization
+                                .authorizeHttpRequests(authz -> authz
+                                                // enable access to root url for all users for AWS HealthCheck
+                                                .requestMatchers("/").permitAll()
+                                                .anyRequest().authenticated() // 1. All requests should be authenticated
+                                )
+                                .httpBasic(withDefaults()) // 2. If a request is not authenticated, a web page is shown
+                                .csrf(csrf -> csrf
+                                                .disable() // 3. Disable CSRF (impacts POST & PUT routes)
+                                );
+
+                return http.build();
+
+        }
+}
